c14f6cbef8033fa861f2827b47e4606dd6bc8e90
Elliot Lewis <elliotleelewis@gmail.com>
2019-09-21 13:27:29 -0400

    feat: initial commit

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	extends: ['@commitlint/config-angular'],
+	rules: {
+		'subject-max-length': [2, 'always', 120],
+		'scope-enum': [2, 'always', []],
+		'scope-empty': [2, 'always'],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'ci',
+				'chore',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'release',
+				'revert',
+				'style',
+				'test',
+			],
+		],
+	},
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PreloadAllModules, RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SharedModule } from './modules/shared/shared.module';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		// Components
+		FooterComponent,
+		NavbarComponent,
+	],
+	imports: [
+		// Angular Modules
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		RouterModule.forRoot(
+			[
+				{
+					path: '',
+					pathMatch: 'full',
+					loadChildren: () =>
+						import('./modules/index/index.module').then(
+							(m) => m.IndexModule,
+						),
+				},
+				{
+					path: 'project',
+					loadChildren: () =>
+						import('./modules/project/project.module').then(
+							(m) => m.ProjectModule,
+						),
+				},
+			],
+			{
+				useHash: true,
+				preloadingStrategy: PreloadAllModules,
+			},
+		),
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: environment.production,
+		}),
+		// External Modules
+		ScrollToModule.forRoot(),
+		// Application Modules
+		SharedModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}

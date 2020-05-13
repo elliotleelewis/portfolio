c5178f9ed117c8d074a08bf09db96ced98015455
Elliot Lewis <elliotleelewis@gmail.com>
2020-05-13 17:47:53 -0400

    build: swapped out purify-css for purgecss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
+const path = require('path');
+const glob = require('glob');
 const dotenvWebpack = require('dotenv-webpack');
+const purgecssWebpackPlugin = require('purgecss-webpack-plugin')
+
+const PATHS = {
+	src: path.join(__dirname, 'src')
+};
 
 module.exports = {
 	plugins: [
 		new dotenvWebpack({
 			systemvars: true,
+		}),
+		new purgecssWebpackPlugin({
+			paths: () => glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
 		}),
 	],
 };

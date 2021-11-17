b6c1cd63594716bf6931557213e597db6ab4b468
Elliot Lewis <elliotleelewis@gmail.com>
2021-11-17 09:25:01 -0500

    chore(deps): upgrade to Angular 13

--- src/test.ts
+++ src/test.ts
@@ -23,6 +23,9 @@ declare const require: {
 getTestBed().initTestEnvironment(
 	BrowserDynamicTestingModule,
 	platformBrowserDynamicTesting(),
+	{
+		teardown: { destroyAfterEach: true },
+	},
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);

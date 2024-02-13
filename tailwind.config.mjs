ae77bba8be057f213b57b7b5c18d98c3b00a8241
Elliot Lewis <17044667+elliotleelewis@users.noreply.github.com>
2024-02-13 14:08:58 -0800

    feat: structured content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
+import tailwindcssBgPatterns from 'tailwindcss-bg-patterns';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	plugins: [tailwindcssBgPatterns],
 };

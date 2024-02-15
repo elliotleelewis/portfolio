5ae00df00984cc10e3157d9e4456fecf154b26a4
Elliot Lewis <elliotleelewis@gmail.com>
2024-02-15 15:16:08 -0800

    feat: added GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://elliotleelewis.com',
 	build: {
 		inlineStylesheets: 'always',
 	},
-	integrations: [tailwind()],
+	integrations: [
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+		sitemap(),
+		tailwind(),
+	],
 });
